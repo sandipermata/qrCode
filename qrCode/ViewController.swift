6f9b8070ed30a12d0255cbb7122210b134372ee4
sandipermata <45228664+sandipermata@users.noreply.github.com>
2018-11-22 11:19:00 +0700

    Initial Commit

---
+++ qrCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qrCode
+//
+//  Created by Sandi Permata on 22/11/18.
+//  Copyright © 2018 myProject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
